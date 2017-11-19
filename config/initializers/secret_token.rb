d730b40ccdcba4f987864d95c3ae20f260b93ea1
Tatsuki Okazaki <fnnyxe1339@gmail.com>
2017-11-19 04:20:05 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DomeApp::Application.config.secret_key_base = 'a0cf38639cc00b3bcfbe41300c2ef1e6178340c2bda4d35e958cebd8a2e10bab4c58bccd5f1c3a98f084eb0da7e376bc8e48505db32566d2c1de313d61a71301'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DomeApp::Application.config.session_store :cookie_store, key: '_dome_app_session'
